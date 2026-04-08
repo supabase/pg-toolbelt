446d4cd83f255e1746d8e78f7a415e3c51e5fd02
avallete <andrew.valleteau@supabase.io>
2026-04-08 19:03:15 +0200

    fix: connection timeout error

--- packages/pg-delta/tests/utils.ts
+++ packages/pg-delta/tests/utils.ts
@@ -82,9 +82,11 @@ export function withDbSupabaseIsolated(
     ]);
     const main = createPool(containerMain.getConnectionUri(), {
       onError: suppressShutdownError,
+      connectionTimeoutMillis: 10_000,
     });
     const branch = createPool(containerBranch.getConnectionUri(), {
       onError: suppressShutdownError,
+      connectionTimeoutMillis: 10_000,
     });
 
     try {
